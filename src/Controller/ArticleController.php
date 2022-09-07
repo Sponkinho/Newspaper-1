9845dbb0865e36c9ed360c615b67159ea1433fdf
BesokDev <74706532+BesokDev@users.noreply.github.com>
2022-09-07 16:57:21 +0200

    Add showArticle() + showArticlesFromCategories()

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/voir-articles/{alias}', name: 'show_articles_from_category', methods: ['GET'])]
+    public function showArticlesFromCategory(Category $category, ArticleRepository $repository): Response
+    {
+        $articles = $repository->findBy([
+            'deletedAt' => null,
+            'category' => $category->getId()
+        ]);
+
+        return $this->render('article/show_articles_from_categories.html.twig', [
+            'articles' => $articles,
+            'category' => $category
+        ]);
+    }
+
+    #[Route('{cat_alias}/{art_alias}_{id}', name: 'show_article', methods: ['GET'])]
+    public function showArticle(Article $article): Response
+    {
+        return $this->render('article/show_article.html.twig', [
+            'article' => $article
+        ]);
+    }
+}
